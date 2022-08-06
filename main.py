801ac12f318f41f66f7ae0b27a7862a18fc02808
Prabesh Shrestha <creatorworld329@gmail.com>
2022-08-06 18:35:46 +0545

    theme wise api responce

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from core.recommendation import *
 
 from fastapi import FastAPI
-
+from enum import Enum
 app = FastAPI()
-
-
 new_df, similarity = recommender()
+
+
+
+
+class Theme(str, Enum):
+    historic = "historic"
+    religious = "religious"
+    park = "park"
+    temple = "temple"
+
+    
 
 
 @app.get("/maris")
@@ -23,6 +32,29 @@ async def retrain():
 
 @app.get("/rec/{place}")
 async def rec(place):
-
     return recommend(place, similarity, new_df)
+
+@app.get("/{theme}")
+async def theme_res(theme: Theme):
+    temp = []
+
+    with open("data/places.json",'r') as file:
+        json_data = json.load(file)
+    for place in json_data['attractions']:
+
+        if theme in place['theme']:
+            temp_data = {
+                    "name": place['name'],
+                    "location": place['location'],
+                    "cords":  place['cords'],
+                    "theme": place['theme'],
+                    "image": place['image'],
+                    "des": place['des']                        
+                    }
+            temp.append(temp_data)
+
+
+
+    return temp
+
 
